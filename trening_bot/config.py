5c0c3e16c259556eb18713434e5fbf86e584f5c9
PavelVNazarov <19pavel76@mail.ru>
2024-11-29 17:29:52 +0500

    Update config.py

--- trening_bot/config.py
+++ trening_bot/config.py
@@ -1,3 +1,12 @@
 # config.py
-API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
-CHANNEL_ID = '@your_channel_id'  # Укажите ID Вашего канала
+def load_config():
+    config = {}
+    with open('config.txt', 'r') as f:
+        for line in f:
+            key, value = line.strip().split('=')
+            config[key] = value
+    return config
+
+config = load_config()
+API_TOKEN = config['API_TOKEN']
+CHANNEL_ID = config['CHANNEL_ID']
